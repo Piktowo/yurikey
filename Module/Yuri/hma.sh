a77ad66d04c47a67c5344513e3ae5a84c46f4459
Tam97123 <tamtranduc001@gmail.com>
2026-02-22 15:43:31 +0700

    Update hma.sh

--- Module/Yuri/hma.sh
+++ Module/Yuri/hma.sh
@@ -3,6 +3,7 @@
 # Define important paths and file names
 HMA_DIR="/data/user/0/org.frknkrc44.hma_oss/files"
 REMOTE_URL="https://raw.githubusercontent.com/YurikeyDev/yurikey/refs/heads/main/config.json"
+ORG_PATH="$PATH"
 
 download() {
     PATH=/data/adb/magisk:/data/data/com.termux/files/usr/bin:$PATH
@@ -11,12 +12,11 @@ download() {
     else
         busybox wget -T 10 --no-check-certificate -qO- "$1"
     fi
-    PATH="$PATH"
+    PATH="$ORG_PATH"
 }
 
 mkdir -p "$HMA_DIR"
-ping -c 1 -w 5 raw.githubusercontent.com &>/dev/null || log_message "Error: Unable to connect to raw.githubusercontent.com, please download and add keybox manually!"
-download "$REMOTE_URL" > "$HMA_DIR" || log_message "Error: Keybox download failed, please download and add it manually!"
+download "$REMOTE_URL" > "$HMA_DIR" || log_message "Error: HMA-oss configs download failed, please download and add it manually!"
 
 chmod 777 /data/user/0/org.frknkrc44.hma_oss/files/config.json
 chown u0_a0:u0_a0 /data/user/0/org.frknkrc44.hma_oss/files/config.json
