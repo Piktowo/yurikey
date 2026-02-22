c8fd3ec826f9993f6c4139fd9ad66b85071c17a8
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-02-22 19:40:26 +0700

    refactor(yuri_keybox): Optimize code when fetching keybox

--- Module/Yuri/yuri_keybox.sh
+++ Module/Yuri/yuri_keybox.sh
@@ -3,8 +3,7 @@
 # Define important paths and file names
 TRICKY_DIR="/data/adb/tricky_store"
 REMOTE_URL="https://raw.githubusercontent.com/Yurii0307/yurikey/main/key"
-REMOTE_FILE="$TRICKY_DIR/key"
-BREMOTE_FILE="$TRICKY_DIR/keybox"
+REMOTE_FILE="$TRICKY_DIR/keybox"
 TARGET_FILE="$TRICKY_DIR/keybox.xml"
 BACKUP_FILE="$TRICKY_DIR/keybox.xml.bak"
 DEPENDENCY_MODULE="/data/adb/modules/tricky_store"
@@ -12,12 +11,15 @@ DEPENDENCY_MODULE_UPDATE="/data/adb/modules_update/tricky_store"
 BBIN="/data/adb/Yurikey/bin"
 ORG_PATH="$PATH"
 
+log_message() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) [SET_TARGET] $1"
+}
+log_message "Start"
+
 # Check if Tricky Store module is installed (required dependency)
-if [ -d "$DEPENDENCY_MODULE_UPDATE" ] || [ -d "$DEPENDENCY_MODULE" ]; then
-  ui_print "- Tricky Store installed"
-else
-  ui_print "- Error: Tricky Store module file not found!"
-  ui_print "- Please install Tricky Store before using Yuri Keybox."
+if [ ! -d "$DEPENDENCY_MODULE_UPDATE" ] && [ ! -d "$DEPENDENCY_MODULE" ]; then
+  log_message "Error: Tricky Store module file not found!"
+  log_message "Please install Tricky Store before using Yuri Keybox."
   return 0
 fi
 
@@ -33,10 +35,10 @@ download() {
 
 # Function to download the remote keybox
 get_keybox() {
-    download "$REMOTE_URL" > "$REMOTE_FILE" || ui_print "Error: Keybox download failed, please download and add it manually!"
+    download "$REMOTE_URL" > "$REMOTE_FILE" || log_message "Error: Keybox download failed, please download and add it manually!"
 
     if ! base64 -d "$REMOTE_FILE" > "$BREMOTE_FILE" 2>/dev/null; then
-        ui_print "- Error: Base64 decode failed!"
+        log_message "- Error: Base64 decode failed!"
         rm -f "$REMOTE_FILE"
         return 1
     fi
@@ -47,14 +49,16 @@ get_keybox() {
 
 # Function to update the keybox file
 update_keybox() {
-  ui_print "- Fetching remote keybox..."
+  log_message "Fetching remote keybox..."
   if ! get_keybox; then
-    ui_print "- Failed to fetch keybox!"
+    log_message "Failed to fetch keybox!"
     return
   fi
 }
 
 # Start main logic
+log_message "Writing"
+
 mkdir -p "$TRICKY_DIR" # Make sure the directory exists
 update_keybox          # Begin the update process
 
