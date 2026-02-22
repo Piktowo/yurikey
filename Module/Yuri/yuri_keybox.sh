3f06cdce4178191ab34ebd1aa350ec7c31e2f6ed
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-02-22 23:58:31 +0700

    fix: Correct variable to decode keybox

--- Module/Yuri/yuri_keybox.sh
+++ Module/Yuri/yuri_keybox.sh
@@ -6,6 +6,7 @@ REMOTE_URL="https://raw.githubusercontent.com/Yurii0307/yurikey/main/key"
 REMOTE_FILE="$TRICKY_DIR/keybox"
 TARGET_FILE="$TRICKY_DIR/keybox.xml"
 BACKUP_FILE="$TRICKY_DIR/keybox.xml.bak"
+DECODE_FILE="$TRICKY_DIR/keybox_decode"
 DEPENDENCY_MODULE="/data/adb/modules/tricky_store"
 DEPENDENCY_MODULE_UPDATE="/data/adb/modules_update/tricky_store"
 BBIN="/data/adb/Yurikey/bin"
@@ -23,6 +24,11 @@ if [ ! -d "$DEPENDENCY_MODULE_UPDATE" ] && [ ! -d "$DEPENDENCY_MODULE" ]; then
   return 0
 fi
 
+# Backup keybox before fetching
+if [ -f "$TARGET_FILE" ]; then
+  mv "$TARGET_FILE" "$BACKUP_FILE"
+fi
+
 download() {
     PATH=/data/adb/magisk:/data/data/com.termux/files/usr/bin:$PATH
     if command -v curl >/dev/null 2>&1; then
@@ -37,19 +43,20 @@ download() {
 get_keybox() {
     download "$REMOTE_URL" > "$REMOTE_FILE" || log_message "Error: Keybox download failed, please download and add it manually!"
 
-    if ! base64 -d "$REMOTE_FILE" > "$BREMOTE_FILE" 2>/dev/null; then
-        log_message "- Error: Base64 decode failed!"
+    if ! base64 -d "$REMOTE_FILE" > "$DECODE_FILE" 2>/dev/null; then
+        log_message "Error: Base64 decode failed!"
         rm -f "$REMOTE_FILE"
+        mv "$BACKUP_FILE" "$TARGET_FILE"
         return 1
     fi
 
     rm -f "$REMOTE_FILE"
+    mv "$DECODE_FILE" "$TARGET_FILE"
     return 0
 }
 
 # Function to update the keybox file
 update_keybox() {
-  log_message "Fetching remote keybox..."
   if ! get_keybox; then
     log_message "Failed to fetch keybox!"
     return
